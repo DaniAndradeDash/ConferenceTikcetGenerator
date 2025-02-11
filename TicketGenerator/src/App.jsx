2b4e47e9f71a55eebb14d2bde8a2c5d0ce88ad5e
Daniel Andrade <daniandrademty@gmail.com>
2025-02-10 22:18:23 -0600

    add backgrounds
    Se agregan imagenes para el background y titulo, se deja pendiente el formulario.

--- TicketGenerator/src/App.jsx
+++ TicketGenerator/src/App.jsx
@@ -4,7 +4,11 @@ function App() {
 
   return (
     <>
-
+      <div className="logoTittle"></div>
+      <div className="lineBottom"></div>
+      <div className="lineTop"></div>
+      <div className="circule"></div>
+      <div className="linesTop"></div>
     </>
   )
 }

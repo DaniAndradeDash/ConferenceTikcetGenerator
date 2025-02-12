5731311a4a5c53dcbf229aaac0aad4cb018cc921
Daniel Andrade <daniandrademty@gmail.com>
2025-02-11 21:57:33 -0600

    add styles
    Se agregan estilos para realizar cajas donde iran inputs y la carga de la imagen

--- TicketGenerator/src/App.jsx
+++ TicketGenerator/src/App.jsx
@@ -1,7 +1,26 @@
-import { Form, Button, Stack, Container, Row, Col } from 'react-bootstrap'
+import { Form, Button, Image } from 'react-bootstrap'
+import { useState } from "react";
+import logoUpload from './assets/images/icon-upload.svg'
 import './App.css'
 
 function App() {
+
+  const [fileName, setFileName] = useState("");
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    if (file) setFileName(file.name);
+  };
+
+  const handleDragOver = (event) => {
+    event.preventDefault();
+  };
+
+  const handleDrop = (event) => {
+    event.preventDefault();
+    const file = event.dataTransfer.files[0];
+    if (file) setFileName(file.name);
+  };
 
   return (
     <>
@@ -10,43 +29,61 @@ function App() {
       <div className="lineTop"></div>
       <div className="circule"></div>
       <div className="linesTop"></div>
-      <div className="tittles text-center">
-        <h1>Your Journey to Coding Conf 2025 Starts Here!</h1>
-        <p>Secure your spot at next year's biggest coding conference</p>
-      </div>
-      <div className="formsTicket">
-        <Form>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-            <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
-            </Form.Text>
-          </Form.Group>
+      <div className="general">
+        <div className="tittles">
+          <h1 className='text-center' style={{ color: "#fdfdfd" }}>Your Journey to Coding Conf 2025 Starts Here!</h1>
+          <p className='text-center' style={{ color: "#fdfdfd" }}>Secure your spot at next year's biggest coding conference</p>
+        </div>
+        <div className="formsTicket">
+          <Form>
+            <Form.Group controlId="formFile" className="mb-3">
+              <Form.Label style={{ color: "#fdfdfd" }}>Upload Avatar</Form.Label>
+              <div
+                className="upload-box"
+                onDragOver={handleDragOver}
+                onDrop={handleDrop}
+                onClick={() => document.getElementById("fileInput").click()}
+              >
+                <Image src={logoUpload} />
+                {/*<FaUpload className='align-self-center' size={40} color="#fdfdfd" />*/}
+                <p style={{ color: "#fdfdfd" }}>
+                  {fileName || "Arrastra o da clic para cargar tu avatar"}
+                </p>
+              </div>
+              <input
+                id="fileInput"
+                type="file"
+                style={{ display: "none" }}
+                onChange={handleFileChange}
+                accept=".jpg,.png"
+              />
+              <Form.Text className="text-muted">
+                Upload your photo (.jpg or .png, max size: 500kb)
+              </Form.Text>
+            </Form.Group>
 
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Check me out" />
-          </Form.Group>
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>
-        </Form>
+            {/* Aqui se empieza a ver la segunda parte del formulario */}
+
+            <Form.Group className="mb-2" controlId="formBasicEmail">
+              {/*<Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>*/}
+              <Form.Label style={{ color: "#fdfdfd" }}>Full Name</Form.Label>
+              <Form.Control className='mb-3' type="text" placeholder="Name" />
+              <Form.Label style={{ color: "#fdfdfd" }}>Email address</Form.Label>
+              <Form.Control className='mb-3' type="email" placeholder="Enter email" />
+              <Form.Label style={{ color: "#fdfdfd" }}>GitHub Username</Form.Label>
+              <Form.Control className='mb-3' type="text" placeholder="@yourusername" />
+              <br />
+            </Form.Group>
+
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </div>
       </div>
 
-      <div className="other">
-        <Stack direction="horizontal" gap={2}>
-          <Button as="a" variant="primary">
-            Button as link
-          </Button>
-          <Button as="a" variant="success">
-            Button as link
-          </Button>
-        </Stack>;
-      </div>
     </>
   )
 }
